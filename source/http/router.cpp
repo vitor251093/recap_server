0cf41c53d09acafc108dfe1675844e7827821001
Vitor <vitor251093@gmail.com>
2019-07-06 13:22:09 -0300

    Revert "Router should decode e-mails"
    This reverts commit 7e7e6050abf79b0138f40a02b56c29a3a4d976ad.

--- source/http/router.cpp
+++ source/http/router.cpp
@@ -111,46 +111,10 @@ namespace HTTP {
 		mRegExpr = std::regex(mPath);
 	}
 
-	bool urlDecode(const std::string& in, std::string& out) {
-		// Reference:
-		// https://www.boost.org/doc/libs/1_47_0/doc/html/boost_asio/example/http/server3/request_handler.cpp
-		
-		out.clear();
-		out.reserve(in.size());
-		for (std::size_t i = 0; i < in.size(); ++i) {
-			if (in[i] == '%') {
-				if (i + 3 <= in.size()) {
-					int value = 0;
-					std::istringstream is(in.substr(i + 1, 2));
-					if (is >> std::hex >> value) {
-						out += static_cast<char>(value);
-						i += 2;
-					} else {
-						return false;
-					}
-				} else {
-					return false;
-				}
-			}
-			else if (in[i] == '+') {
-				out += ' ';
-			} else {
-				out += in[i];
-			}
-		}
-		return true;
-	}
-
 	// Router
 	bool Router::run(Session& session, Response& response) {
 		decltype(auto) request = session.get_request();
-		
-		std::string originalUri = request.data.target().to_string();
-		std::string uri;
-		if (!url_decode(originalUri, uri)) {
-			return false;
-		}
-		request.uri.parse(uri);
+		request.uri.parse(request.data.target().to_string());
 
 		for (const auto& route : mRoutes) {
 			if (route.mMethod == request.data.method() && route.equals(request.uri.resource())) {
