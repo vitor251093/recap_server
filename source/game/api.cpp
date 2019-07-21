ea0aa03a8c1a116ab98e30afb9aa17af5d1a6845
Vitor <vitor251093@gmail.com>
2019-07-21 19:18:58 -0300

    Fixed panel page path

--- source/game/api.cpp
+++ source/game/api.cpp
@@ -381,7 +381,7 @@ version = 1
 		});
 
 		router->add("/panel", { boost::beast::http::verb::get, boost::beast::http::verb::post }, [this](HTTP::Session& session, HTTP::Response& response) {
-			responseWithFileInStorageAtPath(session, response, "/www/" + Config::Get(CONFIG_DARKSPORE_INDEX_PAGE_PATH));
+			responseWithFileInStorageAtPath(session, response, "/www/panel/index.html";
 		});
 
 		router->add("/panel/([/a-zA-Z0-9_.]+)", { boost::beast::http::verb::get, boost::beast::http::verb::post }, [this](HTTP::Session& session, HTTP::Response& response) {
