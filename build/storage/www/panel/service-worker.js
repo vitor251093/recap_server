8fedca778a1c378d9b118333a420850e43a05cd5
Vitor <vitor251093@gmail.com>
2019-07-23 22:19:12 -0300

    Adjust to fix the homepage

--- build/storage/www/panel/service-worker.js
+++ build/storage/www/panel/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04ff698151d39754e552ee76191f7e6e.js"
+  "/panel/precache-manifest.24296f4773c5823d5420488c17280451.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/panel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
