fa564c9870efb394cc6bce0d5837cea877b2e112
Vitor <vitor251093@gmail.com>
2019-08-06 21:42:31 -0300

    toggleClass should work now, and that includes modal

--- build/storage/www/ingame/js/jquery.adapter.js
+++ build/storage/www/ingame/js/jquery.adapter.js
@@ -5,19 +5,11 @@ jQuery.fn.toggleClass = function(className, enable) {
     var hasClass = jQuery.fn.hasClass.apply(this, [className]);
 
     if (hasClass && (enable !== true)) {
-        var classList = this.className.split(" ");
-
-        var index = classList.indexOf(className);
-        while (index > -1) {
-            classList.splice(index, 1);
-            index = classList.indexOf(className);
-        }
-
-        this.className = classList.join(" ");
+        jQuery.fn.removeClass.apply(this, [className]);
         return;
     } 
 
     if (!hasClass && (enable !== false)) {
-        this.className += " " + className;
+        jQuery.fn.addClass.apply(this, [className]);
     }
 };
