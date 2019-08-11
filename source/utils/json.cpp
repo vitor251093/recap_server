e943527d81ed646a28500809947084ae0d990da3
Vitor <vitor251093@gmail.com>
2019-08-11 02:24:08 -0300

    getters now should be able to return null

--- source/utils/json.cpp
+++ source/utils/json.cpp
@@ -10,30 +10,37 @@
 namespace utils {
 
     jsonObject jsonDocumentObject::GetObject(const std::string& label) {
+        if (!rapidDocument.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return jsonObject(rapidAllocator, rapidDocument.GetObject()[rapidLabel]);
     }
     jsonArray jsonDocumentObject::GetArray(const std::string& label) {
+        if (!rapidDocument.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return jsonArray(rapidAllocator, rapidDocument.GetObject()[rapidLabel]);
     }
     bool jsonDocumentObject::GetBool(const std::string& label) {
+        if (!rapidDocument.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidDocument.GetObject()[rapidLabel].GetBool();
     }
     double jsonDocumentObject::GetDouble(const std::string& label) {
+        if (!rapidDocument.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidDocument.GetObject()[rapidLabel].GetDouble();
     }
     uint32_t jsonDocumentObject::GetUint(const std::string& label) {
+        if (!rapidDocument.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidDocument.GetObject()[rapidLabel].GetUint();
     }
     uint64_t jsonDocumentObject::GetUint64(const std::string& label) {
+        if (!rapidDocument.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidDocument.GetObject()[rapidLabel].GetUint64();
     }
     std::string jsonDocumentObject::GetString(const std::string& label) {
+        if (!rapidDocument.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidDocument.GetObject()[rapidLabel].GetString();
     }
@@ -89,24 +96,31 @@ namespace utils {
 
 
     jsonObject jsonDocumentArray::GetObject(rapidjson::SizeType index) {
+        if (index >= rapidDocument.Size()) return NULL;
         return jsonObject(rapidAllocator, rapidDocument.GetArray()[index]);
     }
     jsonArray jsonDocumentArray::GetArray(rapidjson::SizeType index) {
+        if (index >= rapidDocument.Size()) return NULL;
         return jsonArray(rapidAllocator, rapidDocument.GetArray()[index]);
     }
     bool jsonDocumentArray::GetBool(rapidjson::SizeType index) {
+        if (index >= rapidDocument.Size()) return NULL;
         return rapidDocument.GetArray()[index].GetBool();
     }
     double jsonDocumentArray::GetDouble(rapidjson::SizeType index) {
+        if (index >= rapidDocument.Size()) return NULL;
         return rapidDocument.GetArray()[index].GetDouble();
     }
     uint32_t jsonDocumentArray::GetUint(rapidjson::SizeType index) {
+        if (index >= rapidDocument.Size()) return NULL;
         return rapidDocument.GetArray()[index].GetUint();
     }
     uint64_t jsonDocumentArray::GetUint64(rapidjson::SizeType index) {
+        if (index >= rapidDocument.Size()) return NULL;
         return rapidDocument.GetArray()[index].GetUint64();
     }
     std::string jsonDocumentArray::GetString(rapidjson::SizeType index) {
+        if (index >= rapidDocument.Size()) return NULL;
         return rapidDocument.GetArray()[index].GetString();
     }
     jsonObject jsonDocumentArray::NewObject() {
@@ -155,30 +169,37 @@ namespace utils {
 
     
     jsonObject jsonObject::GetObject(const std::string& label) {
+        if (!rapidValue.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return jsonObject(rapidAllocator, rapidValue.GetObject()[rapidLabel]);
     }
     jsonArray jsonObject::GetArray(const std::string& label) {
+        if (!rapidValue.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return jsonArray(rapidAllocator, rapidValue.GetObject()[rapidLabel]);
     }
     bool jsonObject::GetBool(const std::string& label) {
+        if (!rapidValue.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidValue.GetObject()[rapidLabel].GetBool();
     }
     double jsonObject::GetDouble(const std::string& label) {
+        if (!rapidValue.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidValue.GetObject()[rapidLabel].GetDouble();
     }
     uint32_t jsonObject::GetUint(const std::string& label) {
+        if (!rapidValue.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidValue.GetObject()[rapidLabel].GetUint();
     }
     uint64_t jsonObject::GetUint64(const std::string& label) {
+        if (!rapidValue.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidValue.GetObject()[rapidLabel].GetUint64();
     }
     std::string jsonObject::GetString(const std::string& label) {
+        if (!rapidValue.HasMember(label)) return NULL;
         auto rapidLabel = rapidjson::Value{}.SetString(label.c_str(), label.length(), rapidAllocator);
         return rapidValue.GetObject()[rapidLabel].GetString();
     }
@@ -225,24 +246,31 @@ namespace utils {
 
 
     jsonObject jsonArray::GetObject(rapidjson::SizeType index) {
+        if (index >= rapidValue.Size()) return NULL;
         return jsonObject(rapidAllocator, rapidValue.GetArray()[index]);
     }
     jsonArray jsonArray::GetArray(rapidjson::SizeType index) {
+        if (index >= rapidValue.Size()) return NULL;
         return jsonArray(rapidAllocator, rapidValue.GetArray()[index]);
     }
     bool jsonArray::GetBool(rapidjson::SizeType index) {
+        if (index >= rapidValue.Size()) return NULL;
         return rapidValue.GetArray()[index].GetBool();
     }
     double jsonArray::GetDouble(rapidjson::SizeType index) {
+        if (index >= rapidValue.Size()) return NULL;
         return rapidValue.GetArray()[index].GetDouble();
     }
     uint32_t jsonArray::GetUint(rapidjson::SizeType index) {
+        if (index >= rapidValue.Size()) return NULL;
         return rapidValue.GetArray()[index].GetUint();
     }
     uint64_t jsonArray::GetUint64(rapidjson::SizeType index) {
+        if (index >= rapidValue.Size()) return NULL;
         return rapidValue.GetArray()[index].GetUint64();
     }
     std::string jsonArray::GetString(rapidjson::SizeType index) {
+        if (index >= rapidValue.Size()) return NULL;
         return rapidValue.GetArray()[index].GetString();
     }
     jsonObject jsonArray::NewObject() {
