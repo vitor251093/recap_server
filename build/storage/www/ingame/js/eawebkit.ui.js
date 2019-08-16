a14c93b1ff291df744f531738088c376c6fa7c3d
vitor251093 <vitor251093@gmail.com>
2019-08-15 22:19:01 -0300

    eawebkit.ui should support class selectors

--- build/storage/www/ingame/js/eawebkit.ui.js
+++ build/storage/www/ingame/js/eawebkit.ui.js
@@ -2,16 +2,23 @@
 var UI = function(domId) {
 	return {
 		dom: function() {
-			return document.getElementById(domId.substr(1));
+			var prefix = domId.substr(0,1);
+			var realId = domId.substr(1);
+			if (prefix === '#') return [document.getElementById(domId.substr(1))];
+			if (prefix === '.') return  document.getElementsByClassName(domId.substr(1));
+			return [];
 		},
 		show: function(show) {
-			this.dom().style.visibility = (show === undefined || show === true) ? "visible" : "hidden";
+			var doms = this.dom();
+			for (var i = 0; i < doms.length; i++) doms[i].style.visibility = (show === undefined || show === true) ? "visible" : "hidden";
 		},
 		hide: function(hide) {
-			this.dom().style.visibility = (hide === undefined || hide === true) ? "hidden" : "visible";
+			var doms = this.dom();
+			for (var i = 0; i < doms.length; i++) doms[i].style.visibility = (hide === undefined || hide === true) ? "hidden" : "visible";
 		},
 		text: function(text) {
-			this.dom().innerHTML = text;
+			var doms = this.dom();
+			for (var i = 0; i < doms.length; i++) doms[i].innerHTML = text;
 		}
 	};
 };
