29932843519415e89618c66296480a17d6eb758b
Vitor <vitor251093@gmail.com>
2019-09-05 21:16:43 -0300

    Revert "Creature CPP should use JSON namespace methods only"
    This reverts commit 2b0f1702d42fc843dd177c618333a6263fa8a627.

--- source/utils/json.h
+++ source/utils/json.h
@@ -19,12 +19,6 @@ namespace utils {
         rapidjson::Value NewObject();
         rapidjson::Value NewArray();
 
-        std::string GetString(rapidjson::Document& node, const rapidjson::ValueType::Ch& label);
-        bool        GetBool(  rapidjson::Document& node, const rapidjson::ValueType::Ch& label);
-        double      GetDouble(rapidjson::Document& node, const rapidjson::ValueType::Ch& label);
-        uint32_t    GetUint(  rapidjson::Document& node, const rapidjson::ValueType::Ch& label);
-        uint64_t    GetUint64(rapidjson::Document& node, const rapidjson::ValueType::Ch& label);
-        
         void Set(rapidjson::Document& node, const std::string& label, rapidjson::Value& value);
         void Set(rapidjson::Document& node, const std::string& label, const std::string& value);
         void Set(rapidjson::Document& node, const std::string& label, bool value);
@@ -35,18 +29,12 @@ namespace utils {
         void Add(rapidjson::Document& node, rapidjson::Value& value);
         void Add(rapidjson::Document& node, const std::string& value);
 
-        std::string GetString(rapidjson::Value& node, const rapidjson::ValueType::Ch& label);
-        bool        GetBool(  rapidjson::Value& node, const rapidjson::ValueType::Ch& label);
-        double      GetDouble(rapidjson::Value& node, const rapidjson::ValueType::Ch& label);
-        uint32_t    GetUint(  rapidjson::Value& node, const rapidjson::ValueType::Ch& label);
-        uint64_t    GetUint64(rapidjson::Value& node, const rapidjson::ValueType::Ch& label);
-
-        void Set(rapidjson::Value& node, const std::string& label, rapidjson::Value& value,  rapidjson::Document::AllocatorType& allocator);
+        void Set(rapidjson::Value& node, const std::string& label, rapidjson::Value& value, rapidjson::Document::AllocatorType& allocator);
         void Set(rapidjson::Value& node, const std::string& label, const std::string& value, rapidjson::Document::AllocatorType& allocator);
-        void Set(rapidjson::Value& node, const std::string& label, bool value,               rapidjson::Document::AllocatorType& allocator);
-        void Set(rapidjson::Value& node, const std::string& label, double value,             rapidjson::Document::AllocatorType& allocator);
-        void Set(rapidjson::Value& node, const std::string& label, uint32_t value,           rapidjson::Document::AllocatorType& allocator);
-        void Set(rapidjson::Value& node, const std::string& label, uint64_t value,           rapidjson::Document::AllocatorType& allocator);
+        void Set(rapidjson::Value& node, const std::string& label, bool value, rapidjson::Document::AllocatorType& allocator);
+        void Set(rapidjson::Value& node, const std::string& label, double value, rapidjson::Document::AllocatorType& allocator);
+        void Set(rapidjson::Value& node, const std::string& label, uint32_t value, rapidjson::Document::AllocatorType& allocator);
+        void Set(rapidjson::Value& node, const std::string& label, uint64_t value, rapidjson::Document::AllocatorType& allocator);
         
         void Add(rapidjson::Value& node, rapidjson::Value& value, rapidjson::Document::AllocatorType& allocator);
         void Add(rapidjson::Value& node, const std::string& value, rapidjson::Document::AllocatorType& allocator);
