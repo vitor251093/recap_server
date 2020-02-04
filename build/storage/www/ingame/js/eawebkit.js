42b9828318fea1ccc4978c0d186d433ac65ec859
vitor251093 <vitor251093@gmail.com>
2020-02-04 00:09:03 -0300

    eawebkit.js now should be more similar to jQuery

--- build/storage/www/ingame/js/eawebkit.js
+++ build/storage/www/ingame/js/eawebkit.js
@@ -3,8 +3,18 @@ var Utils = {
 	isEAWebKit: function() {
 		return navigator.userAgent.indexOf("EAWebKit") != -1;
 	},
+	filter: function(array, func) {
+		var newArray = [];
+		for (var i = 0; i < array.length; i++) if (func(array[i], i)) newArray.push(array[i]);
+		return newArray;
+	},
 	forEach: function(array, func) {
-		for (var i = 0; i < array.length; i++) func(array[i])
+		for (var i = 0; i < array.length; i++) func(array[i], i)
+	},
+	map: function(array, func) {
+		var newArray = [];
+		for (var i = 0; i < array.length; i++) newArray.push(func(array[i], i));
+		return newArray;
 	},
 	type: function(obj) {
 		return Object.prototype.toString.call(obj);
@@ -17,42 +27,133 @@ var Utils = {
 	}
 };
 
-var UI = function(domId) {
-	return {
-		dom: function() {
-			if (Utils.isString(domId)) {
-				var prefix = domId.substr(0,1);
-				var realId = domId.substr(1);
-				if (prefix === '#') return [document.getElementById(realId)];
-				if (prefix === '.') return  document.getElementsByClassName(realId);
-				return [];
-			}
-			if (Utils.isArray(domId)) {
-				return domId;
-			}
-			return [domId];
-		},
-		show: function(show) {
-			this.style({visibility: ((show === undefined || show === true) ? "visible" : "hidden")});
-		},
-		hide: function(hide) {
-			this.style({visibility: ((hide === undefined || hide === true) ? "hidden" : "visible")});
-		},
-		text: function(text) {
-			Utils.forEach(this.dom(),function(dom){dom.innerHTML = text});
-		},
-		toggle: function(value) {
-			Utils.forEach(this.dom(),function(dom){
-				if (dom.tagName.toUpperCase() === "INPUT" && dom.type.toUpperCase() === "CHECKBOX") {
-					dom.checked = (value === true || value === false) ? value : !dom.checked;
-					return;
-				}
-			});
-		},
-		style: function(obj) {
-			Utils.forEach(this.dom(),function(dom){for (var p in obj) dom.style[p] = obj[p]});
-		}
+var _arrayOfElementsWithIdentifier = function(domId) {
+	var _dom = null;
+	if (Utils.isString(domId)) {
+		var prefix = domId.substr(0,1);
+		var realId = domId.substr(1);
+		     if (prefix === '#') _dom = [document.getElementById(realId)];
+		else if (prefix === '.') _dom =  document.getElementsByClassName(realId);
+		else _dom = [];
+	}
+	else if (Utils.isArray(domId)) {
+		_dom = domId;
+	} 
+	else {
+		_dom = [domId];
+	}
+	return _dom;
+};
+var _arrayOfClassesWithClassesString = function(classesString) {
+	var classes = classesString.split(" ");
+	while(classes.indexOf('') != -1) classes.splice(classes.indexOf(''), 1);
+	return classes;
+}
+
+var $ = function(domId) {
+	var obj = {};
+
+	var _dom = _arrayOfElementsWithIdentifier(domId);
+	Utils.forEach(_dom,function(dom, index){ obj[index] = dom; });
+	obj._dom = _dom;
+	obj.length = _dom.length;
+
+	obj.add = function(newDomId) {
+		var el = $(domId);
+		el._dom.push(_arrayOfElementsWithIdentifier(newDomId));
+		Utils.forEach(el._dom,function(dom, index){ el[index] = dom; });
+		el.length = el._dom.length;
+		return el;
 	};
+	obj.addClass = function(className) {
+		Utils.forEach(this._dom,function(dom){ 
+			var classes = _arrayOfClassesWithClassesString(dom.className);
+			classes = classes.concat(_arrayOfClassesWithClassesString(className));
+			classes = Utils.filter(classes, function(item, pos) {
+				return classes.indexOf(item) == pos;
+			})
+			dom.className = classes.join(" "); 
+		});
+		return this;
+	};
+	obj.removeClass = function(className) {
+		Utils.forEach(this._dom,function(dom){ 
+			var classes = _arrayOfClassesWithClassesString(dom.className);
+			var classesToRemove = _arrayOfClassesWithClassesString(className);
+			classes = Utils.filter(classes, function(item) {
+				return classesToRemove.indexOf(item) == -1;
+			})
+			dom.className = classes.join(" "); 
+		});
+		return this;
+	};
+	obj.attr = function(attrName, attrValue) {
+		if (attrValue === undefined) {
+			if (this._dom.length == 0) return null;
+			return this._dom[0][attrName];
+		}
+		Utils.forEach(this._dom,function(dom){dom[attrName] = attrValue});
+		return this;
+	};
+	obj.children = function() {
+		var newArray = [];
+		Utils.forEach(this._dom,function(dom){ newArray = newArray.concat(dom.childNodes) });
+		return $(newArray);
+	};
+	obj.click = function(func) {
+		Utils.forEach(this._dom,function(dom){ dom.onmouseup = func });
+		return this;
+	};
+	obj.css = function(name, value) {
+		if (value === undefined) {
+			if (this._dom.length == 0) return null;
+			return this._dom[0].style[name];
+		}
+		Utils.forEach(this._dom,function(dom){ dom.style[name] = value; });
+		return this;
+	};
+	obj.empty = function() {		
+		Utils.forEach(this._dom,function(dom){ 
+			while (dom.childNodes.length > 0) dom.removeChild(dom.childNodes[0]); 
+		});
+		return this;
+	};
+	obj.get = function(index) {
+		return this._dom[index];
+	};
+	obj.show = function(show) {
+		return this.css('visibility', ((show === undefined || show === true) ? "visible" : "hidden"));
+	};
+	obj.hide = function(hide) {
+		return this.css('visibility', ((hide === undefined || hide === true) ? "hidden" : "visible"));
+	};
+	obj.html = function(text) {
+		if (text === undefined) {
+			if (this._dom.length == 0) return null;
+			return this._dom[0].innerHTML;
+		}
+		Utils.forEach(this._dom,function(dom){dom.innerHTML = text});
+		return this;
+	};
+	obj.toggleChecked = function(value) {
+		Utils.forEach(this._dom,function(dom){
+			var tagName = dom.tagName.toUpperCase();
+			var type = dom.type.toUpperCase();
+			if (tagName === "INPUT" && type === "CHECKBOX") {
+				dom.checked = (value === true || value === false) ? value : !dom.checked;
+			}
+		});
+		return this;
+	};
+	obj.val = function(value) {
+		if (value === undefined) {
+			if (this._dom.length == 0) return null;
+			return this._dom[0].value;
+		}
+		Utils.forEach(this._dom,function(dom){ dom.value = value; });
+		return this;
+	};
+	return obj;
 };
 
 var HTTP = {
