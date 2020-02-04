2917291db32cd58ab6b65688e39642c5afdbc3d3
vitor251093 <vitor251093@gmail.com>
2020-02-03 22:26:10 -0300

    Removing uneeded stuff

--- build/storage/www/ingame/js/eawebkit.js
+++ build/storage/www/ingame/js/eawebkit.js
@@ -52,26 +52,6 @@ var UI = function(domId) {
 		style: function(obj) {
 			Utils.forEach(this.dom(),function(dom){for (var p in obj) dom.style[p] = obj[p]});
 		}
-		// animate: function(style, fromValue, toValue, time, framerate, valueParser) {
-		// 	if (framerate === undefined) framerate = 30;
-			
-		// 	var value = fromValue;
-		// 	var actionChange = (((fromValue - value)*1.0)/framerate)/(time/1000.0);
-		// 	var actionTime = 1000.0/framerate;
-
-		// 	if (value < toValue) {
-		// 		value += actionChange;
-
-		// 		var doms = this.dom();
-		// 		setTimeout(function(){
-		// 			UI(doms).animate(style, value, toValue, time - actionTime, framerate);
-		// 		}, actionTime);
-		// 	}
-			
-		// 	var styleObj = {};
-		// 	styleObj[style] = (valueParser === undefined) ? value : valueParser(value);
-		// 	this.style(styleObj);
-		// }
 	};
 };
 
